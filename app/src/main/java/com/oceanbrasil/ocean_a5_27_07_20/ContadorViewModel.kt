cfd4f2e1f3ad031adcbc2056348389b60fd958f6
Paulo Salvatore <paulo@salvatore.tech>
2020-07-27 20:30:29 -0300

    Initial commit

---
+++ app/src/main/java/com/oceanbrasil/ocean_a5_27_07_20/ContadorViewModel.kt
@@ -0,0 +1,7 @@
+package com.oceanbrasil.ocean_a5_27_07_20
+
+import androidx.lifecycle.ViewModel
+
+class ContadorViewModel : ViewModel() {
+    var i = 0
+}
---
+++ app/src/main/java/com/oceanbrasil/ocean_a5_27_07_20/MainActivity.kt
@@ -0,0 +1,74 @@
+package com.oceanbrasil.ocean_a5_27_07_20
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.lifecycle.ViewModelProvider
+import kotlinx.android.synthetic.main.activity_main.*
+
+class MainActivity : AppCompatActivity() {
+
+    val viewModel by lazy {
+        ViewModelProvider(this).get(ContadorViewModel::class.java)
+    }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        textView.setOnClickListener {
+            viewModel.i++
+
+            atualizarExibicaoTexto()
+        }
+
+        atualizarExibicaoTexto()
+    }
+
+    private fun atualizarExibicaoTexto() {
+        textView.text = "Texto clicado: ${viewModel.i}"
+    }
+
+/*
+// Persistência de dados utilizando savedInstanceState
+var i = 0
+
+override fun onCreate(savedInstanceState: Bundle?) {
+    super.onCreate(savedInstanceState)
+    setContentView(R.layout.activity_main)
+
+    textView.text = "Texto inicial"
+
+    if (i > 0) {
+        atualizarExibicaoTexto()
+    }
+
+    textView.setOnClickListener {
+        i++
+
+        atualizarExibicaoTexto()
+    }
+}
+
+override fun onPostCreate(savedInstanceState: Bundle?) {
+    super.onPostCreate(savedInstanceState)
+
+    if (i > 0) {
+        atualizarExibicaoTexto()
+    }
+}
+
+override fun onSaveInstanceState(outState: Bundle) {
+    outState.putInt("CONTADOR", i)
+    super.onSaveInstanceState(outState)
+}
+
+override fun onRestoreInstanceState(savedInstanceState: Bundle) {
+    super.onRestoreInstanceState(savedInstanceState)
+    i = savedInstanceState.getInt("CONTADOR")
+}
+
+private fun atualizarExibicaoTexto() {
+    textView.text = "Texto clicado: $i"
+}
+*/
+}
